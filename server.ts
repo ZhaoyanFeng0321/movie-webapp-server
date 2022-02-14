d83d17a1d70257ac1562cc62535d4ae995b0f632
kimrine <347642700@qq.com>
2022-02-14 18:54:22 -0500

    modify server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from "mongoose";
 import TuitDao from "./daos/TuitDao";
-import UserDao from "./daos/UserDao";
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
 import bodyParser from "body-parser";
@@ -9,9 +8,11 @@ import bodyParser from "body-parser";
 const app = express();
 mongoose.connect('mongodb+srv://kimrine:kimrine123@cluster0.x1j4c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+app.use(express.json);
 
 const userController = UserController.getInstance(app);
 const tuitController = new TuitController(app, new TuitDao());
