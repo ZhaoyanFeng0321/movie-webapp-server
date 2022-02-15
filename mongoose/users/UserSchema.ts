9c1ca25eb7518d6bc0cf6f05aecadf71b35d3519
kimrine <347642700@qq.com>
2022-02-14 20:10:02 -0500

    UserSchema add _id

--- mongoose/users/UserSchema.ts
+++ mongoose/users/UserSchema.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
+    _id: String,
     username: {type: String, required: true},
     password: {type: String, required: true},
     firstName: String,
