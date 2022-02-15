df8decfb020e5187e4e95e23d6ff1e4ed3f92165
kimrine <347642700@qq.com>
2022-02-14 20:33:48 -0500

    modify UserSchema _id

--- mongoose/users/UserSchema.ts
+++ mongoose/users/UserSchema.ts
@@ -1,7 +1,7 @@
 import mongoose, {Schema} from "mongoose";
+import User from "../../models/users/User";
 
-const UserSchema = new mongoose.Schema({
-    _id: {type: Schema.Types.ObjectId} ,
+const UserSchema = new mongoose.Schema<User>({
     username: {type: String, required: true},
     password: {type: String, required: true},
     firstName: String,
