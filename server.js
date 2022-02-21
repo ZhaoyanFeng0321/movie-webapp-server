f0ad213a0effe3e3b68d36a9bcd6489e36ad2ce1
kimrine <347642700@qq.com>
2022-02-21 14:44:02 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+import mongoose from "mongoose";
+mongoose.connect('mongodb://localhost:27017/tuit-db');
+
+import TuitDao from "./daos/TuitDao";
+import UserDao from "./daos/UserDao";
+
+require('./controllers/UserController')(app,new UserDao());
+require('./controllers/TuitController')(app,new TuitDao());
+
+app.get('/hello', (req, res) =>
+    res.send('Hello World!'));
+
+const PORT = 4000;
+app.listen(PORT);
